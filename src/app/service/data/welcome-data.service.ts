c648d9328e72770955f54a27f0b2e09546077a23
Mohammed Rayyan Shaji <rayyanz95@gmail.com>
2019-08-03 15:35:18 +0530

    Added authentication headers

--- src/app/service/data/welcome-data.service.ts
+++ src/app/service/data/welcome-data.service.ts
@@ -5,6 +5,11 @@ export class HelloWorldBean {
   constructor(public message: string) { }
 }
 
+
+/*export class ByeBean {
+  constructor(public message: string) { }
+}*/
+
 @Injectable({
   providedIn: 'root'
 })
@@ -15,16 +20,28 @@ export class WelcomeDataService {
   constructor(private http: HttpClient) { }
 
   executeHelloWorldBeanService() {
-   return this.http.get <HelloWorldBean>('http://localhost:8080/hello-world'); 
+    return this.http.get<HelloWorldBean>('http://localhost:8080/hello-world'); 
   }
 
   executeHelloWorldBeanServiceWithParameter(name) {
+    let basicAuthHeaderString = this.createBasicAuthenticationHttpHeader();
 
- 
+    let header = new HttpHeaders({
+      Authorization: basicAuthHeaderString
+    })
+    console.log(basicAuthHeaderString)
     return this.http.get<HelloWorldBean>(
-      `http://localhost:8080/hello-world/path/${name}`);
+      `http://localhost:8080/hello-world/path/${name}`,
+      { headers: header });
+    //console.log("Execute Hello World Bean Service")
   }
- 
+
+  createBasicAuthenticationHttpHeader() {
+    let username = 'rayyanshaji'
+    let password = 'kidman'
+    let basicAuthHeaderString = 'Basic ' + window.btoa(username + ':' + password);
+    return basicAuthHeaderString;
+  }
 }
 
 //Access to XMLHttpRequest at 'http://localhost:8080/hello' from origin 'http://localhost:4200' has been blocked by CORS policy: No 'Access-Control-Allow-Origin' header is present on the requested resource.
